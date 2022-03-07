bc35f46d7e745f171ad712febb4886d94de8ac70
杨金明 <yangjinming@newbloc.org>
2022-03-07 15:31:14 +0800

    【删除】删除与框架无关的不合时宜的代码

--- apis/graph.py
+++ apis/graph.py
@@ -28,22 +28,16 @@ class GraphQuery(graphene.ObjectType):
     仅提供入口点，其他信息通过入口点字段关联获取
     """
     user = graphene.Field(UserQuery)
-    reports = graphene.Field(ReportQuery)
-    report = graphene.Field(ReportQuery, rid=graphene.String())
+    demo = graphene.Field(DemoQuery, rid=graphene.String())
 
     def resolve_user(self, info):
         uid = get_jwt_identity()
         query = UserQuery.get_query(info)  # SQLAlchemy query
         return query.filter(User.id == uid).first()
 
-    def resolve_reports(self, info):
-        uid = get_jwt_identity()
-        query = ReportQuery.get_query(info)  # SQLAlchemy query
-        return query.filter(Report.uid == uid).all()
-
     def resolve_report(self, info, rid):
-        query = ReportQuery.get_query(info)  # SQLAlchemy query
-        return query.filter(Report.id == rid).first()
+        query = DemoQuery.get_query(info)  # SQLAlchemy query
+        return query.filter(Demo.id == rid).first()
 
 
 class GraphMutation(graphene.ObjectType):
@@ -52,14 +46,6 @@ class GraphMutation(graphene.ObjectType):
     """
     create_user = CreateUser.Field()
     update_user = UpdateUser.Field()
-    create_credential = CreateCredential.Field()
-    update_credential = UpdateCredential.Field()
-    create_plugin = CreatePlugin.Field()
-    update_plugin = UpdatePlugin.Field()
-    create_region = CreateRegion.Field()
-    update_region = UpdateRegion.Field()
-    create_service = CreateService.Field()
-    update_service = UpdateService.Field()
     delete = GeneralDelete.Field()
 
 
